74cefb034182417beadb0d9499a106ce1f71603f
protocolpenguin <59795922+protocolpenguin@users.noreply.github.com>
2021-01-15 12:36:00 +0000

    Update atombomb.py

--- atombomb.py
+++ atombomb.py
@@ -22,7 +22,7 @@ LOGO = r'''
  /_/    \_\__\___/|_| |_| |_|____/ \___/|_| |_| |_|_.__/ 
                                                          
                                                          
-	https://github.com/SMCbite1024/Atom-bomb
+	https://github.com/protocolpenguin/Atom-bomb
 	   https://twitter.com/SMCbite1024
 '''
 
